a5ae020cc7c960a0b643ede60a82e487b9faebd1
edgarjsr <ejsr08@gmail.com>
2016-10-26 09:41:40 -0400

    module 4: solution

---
+++ module4-solution/script.js
@@ -0,0 +1,18 @@
+(function (window){
+
+	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+	for (var i = 0; i < names.length; i++) {
+
+	  var firstLetter = names[i].charAt(0);
+	  firstLetter = firstLetter.toLowerCase();
+
+	  if (firstLetter == 'j') {
+	    // byeSpeaker.xxxx
+	    byeSpeaker.speak(names[i]);
+	  } else {
+	    // helloSpeaker.xxxx
+	    helloSpeaker.speak(names[i]);
+	  }
+	}
+}) (window);
